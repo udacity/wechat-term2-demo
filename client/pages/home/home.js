6bc84f48c06e8e5bd56d868c50c603281647e9a1
wctjerry <wctjerry@wctjerrydeMacBook-Pro.local>
2018-03-12 14:14:29 +0800

    Establish home page.
    1. Add hot image
    2. Add hot title 春季推荐

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -5,7 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    productList: [{
+      id: 1,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+      name: '商品1',
+      price: 100,
+      source: '国内·广东',
+    }, {
+      id: 2,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
+      name: '商品2',
+      price: 200,
+      source: '国内·广东',
+    }, {
+      id: 3,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product3.jpg',
+      name: '商品3',
+      price: 300,
+      source: '国内·广东',
+    }, {
+      id: 4,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product4.jpg',
+      name: '商品4',
+      price: 400,
+      source: '国内·广东',
+    }, {
+      id: 5,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product5.jpg',
+      name: '商品5',
+      price: 500,
+      source: '国内·广东',
+    }], // 商品列表
   },
 
   /**
