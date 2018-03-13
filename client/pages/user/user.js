6b5244099db75f8d63b7b4f7c394eb096150e128
wctjerry <wctjerry@wctjerrydeMacBook-Pro.local>
2018-03-13 16:09:25 +0800

    Add static pages on user page (before and after login)

--- client/pages/user/user.js
+++ client/pages/user/user.js
@@ -5,8 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    userInfo: null,
+    // userInfo: {
+    //   nickName: "优达学城",
+    //   avatarUrl: "", // 头像 URL 地址
+    // }, // 虚拟数据
   },
+
+  onTapAddress() {
+    wx.showToast({
+      icon: 'none',
+      title: '此功能暂未开放'
+    })
+  },
+
+  onTapKf() {
+    wx.showToast({
+      icon: 'none',
+      title: '此功能暂未开放'
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
