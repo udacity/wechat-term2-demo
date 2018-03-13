9398cdac7932c9f9c232c16c15b2c94abec56359
wctjerry <wctjerry@wctjerrydeMacBook-Pro.local>
2018-03-13 12:22:37 +0800

    Add detail page
    Current data is fake and will be replaced later.

---
+++ client/pages/detail/detail.js
@@ -0,0 +1,72 @@
+// pages/detail/detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    product: {
+      id: 1,
+      image: 'https://product-1256088332.cos.ap-guangzhou.myqcloud.com/product2.jpg',
+      name: '商品',
+      price: 480.5,
+      source: '国内·广东'
+    },
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
