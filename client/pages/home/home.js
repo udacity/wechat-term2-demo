4af09b311b5b1d151d37b8356628c949b024ef3a
wctjerry <wctjerry@wctjerrydeMacBook-Pro.local>
2018-03-14 18:02:13 +0800

    Add product to trolley on home page.

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -45,6 +45,51 @@ Page({
     })
   },
 
+  addToTrolley(event){
+    let productId = event.currentTarget.dataset.id
+    let productList = this.data.productList
+    let product
+
+    for (let i = 0, len = productList.length; i < len; i++) {
+      if (productList[i].id === productId) {
+        product = productList[i]
+        break
+      }
+    }
+
+    if (product){
+      qcloud.request({
+        url: config.service.addTrolley,
+        login: true,
+        method: 'PUT',
+        data: product,
+        success: result => {
+          let data = result.data
+
+          if (!data.code) {
+            wx.showToast({
+              title: '已添加到购物车',
+            })
+          } else {
+            wx.showToast({
+              icon: 'none',
+              title: '添加到购物车失败',
+            })
+          }
+        },
+        fail: () => {
+          wx.showToast({
+            icon: 'none',
+            title: '添加到购物车失败',
+          })
+        }
+      })
+      
+    }
+
+
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
