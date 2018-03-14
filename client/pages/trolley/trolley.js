16d75c7825fafc584c2d4f78d49a3125294dec8c
wctjerry <wctjerry@wctjerrydeMacBook-Pro.local>
2018-03-14 16:34:50 +0800

    Add trolley page with fake data and without loop

--- client/pages/trolley/trolley.js
+++ client/pages/trolley/trolley.js
@@ -10,6 +10,25 @@ Page({
    */
   data: {
     userInfo: null,
+    trolleyList: [{
+      id: 1,
+      name: '商品1',
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+      price: 45,
+      source: '海外·瑞典',
+      count: 1,
+    }, {
+      id: 2,
+      name: '商品2',
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
+      price: 158,
+      source: '海外·新西兰',
+      count: 1,
+    }], // 购物车商品列表
+    trolleyCheckMap: [undefined, true, undefined], // 购物车中选中的id哈希表
+    trolleyAccount: 45, // 购物车结算总价
+    isTrolleyEdit: false, // 购物车是否处于编辑状态
+    isTrolleyTotalCheck: true, // 购物车中商品是否全选
   },
 
   onTapLogin() {
